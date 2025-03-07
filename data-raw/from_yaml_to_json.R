87342ec6529f1641d3454ce7fade930c8595c2d4
douwe <d.molenaar@vu.nl>
2025-03-07 16:37:20 +0100

    remove obsolete file

--- data-raw/from_yaml_to_json.R
+++
@@ -1,8 +0,0 @@
-# Once only
-
-source <- yaml::read_yaml("data-raw/guide_competition_9_3_source.yml")
-
-jsonlite::write_json(source,
-                     path="data-raw/guide_competition_9_3_source.json",
-                     pretty = TRUE,
-                     auto_unbox = TRUE)
