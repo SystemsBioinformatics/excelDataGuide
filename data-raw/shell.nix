2fe6d1619f2d1e2e4f513d1ac3123385322cd146
Douwe Molenaar <d.molenaar@vu.nl>
2025-09-01 14:16:36 +0200

    adjust usage comment

--- data-raw/shell.nix
+++ data-raw/shell.nix
@@ -1,9 +1,10 @@
 # Install the nix package manager if it is not available: https://nixos.org/download/#download-nix
-# This nix shell definition lets you run 'cue' version 0.8.2 under go version go1.22.8 in a nix shell.
-# Start it by running 'nix-shell' in this folder.
+# This nix shell definition lets you run 'cue' in a nix shell.
+# Install it by running the command 'nix-shell' in a terminal in this folder, then run `cue` in the
+# nix shell. Enter 'exit' to exit the nix shell.
 
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
